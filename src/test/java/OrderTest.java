469fb78a497086e0976f759cef32962974f173c2
Olga Tyupina <olga.tyupina@gmail.com>
2019-12-11 16:58:13 +0300

    Initial commit

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+       SelenideElement form = $("[data-test-id]");
+        form.$("[data-test-id=name] input").setValue("Ольга Тюпина");
+        form.$("[data-test-id=phone] input").setValue("+79108747630");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[button__content]").click();
+        $(".order-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+    }
+}
